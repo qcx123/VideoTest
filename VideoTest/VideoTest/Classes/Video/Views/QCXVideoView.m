54f328e4007c3a7fad0eacde513ae083a9aaa02f
乔春晓 <qiaochunxiao@qiaochunxiaodeMacBook-Pro.local>
2018-07-15 22:18:39 +0800

    提交新的

---
+++ VideoTest/VideoTest/Classes/Video/Views/QCXVideoView.m
@@ -0,0 +1,21 @@
+//
+//  QCXVideoView.m
+//  VideoTest
+//
+//  Created by 乔春晓 on 2018/7/15.
+//  Copyright © 2018年 乔春晓. All rights reserved.
+//
+
+#import "QCXVideoView.h"
+
+@implementation QCXVideoView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
